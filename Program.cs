b052b7e1de038a547ee9179284a351a91ec6c9f3
Cam <45856592+CamLag@users.noreply.github.com>
2018-12-20 12:08:22 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,79 +6,90 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string[,] morp = { { "X", "X", " " }, { " ", "X", "O" }, { " ", " ", " " } };
+            string[,] morp = { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
             int I = 0, J = 0;
-            string campGagnant="personne";
+            string campGagnant="match nul!";
+            Boolean traitJoueur = true;
 
-            grid(morp);
+            Grid(morp);
 
 
 
-            while (!(verifyVictory(morp,ref campGagnant)) || !(verifyFull(morp)))
+            while (!(VerifyVictory(morp,ref campGagnant)) && !(VerifyFull(morp)))
             {
-                do
+                if (traitJoueur == true)
                 {
-                    Console.WriteLine("verifyVictory vaut : {0}", verifyVictory(morp, ref campGagnant));
-                    Console.WriteLine("verifyFull vaut : {0}", verifyFull(morp));
-                    Console.WriteLine("Rentrez votre première coordonnée (entre 0 et 2)");
-                    I = playerCoord();
-                    Console.WriteLine("Rentrez votre deuxième coordonnée (entre 0 et 2)");
-                    J = playerCoord();
-                    if (verifyCoord(I, J, morp))
+                    do
                     {
-                        Console.WriteLine("Il y a déjà un symbole ici");
-                    }
-                    else
-                    {
-                        morp[I, J] = "X";
-                        break;
-                    }
-                } while (verifyCoord(I, J, morp));
-
-                grid(morp);
-
-                do
+                        Console.WriteLine("Rentrez votre première coordonnée (entre 0 et 2)");
+                        I = PlayerCoord();
+                        Console.WriteLine("Rentrez votre deuxième coordonnée (entre 0 et 2)");
+                        J = PlayerCoord();
+                        if (VerifyCoord(I, J, morp))
+                        {
+                            Console.WriteLine("Il y a déjà un symbole ici");
+                        }
+                        else
+                        {
+                            morp[I, J] = "X";
+                            break;
+                        }
+                    } while (VerifyCoord(I, J, morp));
+                    traitJoueur = false;
+                    Grid(morp);
+                }
+                else
                 {
-                    I = compCoord();
-                    J = compCoord();
-                    if (verifyCoord(I, J, morp))
+                    do
                     {
-                        Console.WriteLine("je rééssaye");
-                    }
-                    else
-                    {
-                        morp[I, J] = "O";
-                        break;
-                    }
-                } while (verifyCoord(I, J, morp));
-
-                grid(morp);
-
+                        I = CompCoord();
+                        J = CompCoord();
+                        if (VerifyCoord(I, J, morp))
+                        {
+                            Console.WriteLine("je rééssaye");
+                        }
+                        else
+                        {
+                            morp[I, J] = "O";
+                            break;
+                        }
+                    } while (VerifyCoord(I, J, morp));
+                    traitJoueur = true;
+                    Grid(morp);
+                }
             }
-            Console.WriteLine("Le gagna");
+            Console.WriteLine(campGagnant);
 
         }
         //Fonction qui demande une coord au joueur
-        static int playerCoord()
+        static int PlayerCoord()
         {
             int a = -1;
 
             while ((a < 0) || (a > 2))
             {
-                a = Convert.ToInt32(Console.ReadLine());
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Ce n'est pas un nombre!");
+                }
+                if ((a < -1) || (a > 2)) Console.WriteLine("Votre coordonnée doit être comprise entre 0 et 2!");
             }
             return a;
         }
 
         //Fonction qui fait choisir une coordonnée à l'ordi
-        static int compCoord()
+        static int CompCoord()
         {
             Random rand = new Random();
             return rand.Next(0, 3);
         }
 
         //Fonction d'affichage de la grille
-        static void grid(string[,] tab)
+        static void Grid(string[,] tab)
         {
             for (int i = 0; i < tab.GetLength(0); i++)
             {
@@ -89,10 +100,11 @@ namespace ConsoleApp1
                 }
                 Console.WriteLine("|");
             }
+            Console.WriteLine("");
         }
 
         //Fonction qui vérifie si un un élément de la grille est plein
-        static bool verifyCoord(int a, int b, string[,] tab)
+        static bool VerifyCoord(int a, int b, string[,] tab)
         {
             if (tab[a, b] == " ")
             {
@@ -105,10 +117,11 @@ namespace ConsoleApp1
         }
 
         //Fonction qui vérifie si un joueur a gagné
-        static bool verifyVictory(string[,] tab, ref string campGagnant)
+        static bool VerifyVictory(string[,] tab, ref string campGagnant)
         {
             int comptX = 0;
             int comptO = 0;
+            Boolean retour = false;
 
             // Lignes
             for (int i = 0; i < tab.GetLength(0); i++)
@@ -118,19 +131,16 @@ namespace ConsoleApp1
                     if (tab[i, j] == "X") { comptX++; }
                     if (tab[i, j] == "O") { comptO++; }
                 }
-                Console.WriteLine("sur la ligne {0}", i);
-                Console.WriteLine("comptX vaut :{0}", comptX);
-                Console.WriteLine("comptO vaut :{0}", comptO);
 
                 if (comptX == tab.GetLength(1))
                 {
-                    campGagnant = "X";
-                    return true;
+                    campGagnant = "Vous avez gagné!";
+                    retour = true;
                 }
                 if (comptO == tab.GetLength(1))
                 {
-                    campGagnant = "O";
-                    return true;
+                    campGagnant = "J'ai gagné!";
+                    retour = true;
                 }
                 comptX = 0;
                 comptO = 0;
@@ -143,29 +153,64 @@ namespace ConsoleApp1
                     if (tab[i, j] == "X") { comptX++; }
                     if (tab[i, j] == "O") { comptO++; }
                 }
-                Console.WriteLine("sur la colonne {0}", j);
-                Console.WriteLine("comptX vaut :{0}", comptX);
-                Console.WriteLine("comptO vaut :{0}", comptO);
-
                 if (comptX == tab.GetLength(1))
                 {
-                    campGagnant = "X";
-                    return true;
+                    campGagnant = "Vous avez gagné!";
+                    retour = true;
                 }
                 if (comptO == tab.GetLength(1))
                 {
-                    campGagnant = "O";
-                    return true;
+                    campGagnant = "J'ai gagné!";
+                    retour = true;
                 }
                 comptX = 0;
                 comptO = 0;
             }
-            //Diagonales à implémenter
-            return false;
+            //Diagonale 1
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                if (tab[i, i] == "X") { comptX++; }
+                if (tab[i, i] == "O") { comptO++; }
+            }
+            if (comptX == tab.GetLength(1))
+            {
+                campGagnant = "Vous avez gagné!";
+                retour = true;
+            }
+            if (comptO == tab.GetLength(1))
+            {
+                campGagnant = "J'ai gagné!";
+                retour = true;
+            }
+
+            comptX = 0;
+            comptO = 0;
+
+            //Diagonale 2
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                if (tab[i, tab.GetLength(0) - i - 1] == "X") { comptX++; }
+                if (tab[i, tab.GetLength(0) - i - 1] == "O") { comptO++; }
+            }
+            if (comptX == tab.GetLength(1))
+            {
+                campGagnant = "Vous avez gagné!";
+                retour = true;
+            }
+            if (comptO == tab.GetLength(1))
+            {
+                campGagnant = "J'ai gagné!";
+                retour = true;
+            }
+
+            comptX = 0;
+            comptO = 0;
+
+            return retour;
         }
 
         //Fonction qui vérifie si la grille est pleine
-        static bool verifyFull(string[,] tab)
+        static bool VerifyFull(string[,] tab)
         {
             for (int i = 0; i < tab.GetLength(0); i++)
             {
